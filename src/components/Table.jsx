b9ec0b2f62120c032f5233e3463b5f8163079095
Pauts178 <katherine.pautler@gmail.com>
2024-03-15 17:23:49 -0500

    tasks 1-4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,14 +3,21 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+     {/* <tbody>
+    
+     <tr key={sat.id}>
+       <td>{data.name}</td>
+       <td>{data.type}</td>
+       <td>{data.launchDate}</td>
+       <td>{data.orbitType}</td>
      </tr>
-     </tbody>
+     </tbody> */}
    </table>
   );
 };
